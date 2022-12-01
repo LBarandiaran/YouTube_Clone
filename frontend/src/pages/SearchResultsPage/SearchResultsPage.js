cd7b57b52f56ae255f11ea7e2b0bd44ac3ff47c8
unknown <colost72@gmail.com>
2022-11-30 21:54:00 -0700

    Created SearchResultsPage Page but still impcomplete

---
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+
+import axios from "axios";
+
+const SearchResults = () => {
+    
+}
