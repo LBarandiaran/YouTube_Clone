e1f5634a359959d4376ad0b5032fcf49c8b03388
unknown <colost72@gmail.com>
2022-12-08 23:53:31 -0700

    YoutubePage now showing Titles but not Thumbnails, also localKey.js import is not working, still have to use API in public view

--- frontend/src/pages/YouTubePage/YouTubePage.js
+++ frontend/src/pages/YouTubePage/YouTubePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {KEY} from './src/localKey.js'
+// import {KEY} from "./src/localKey.js"
 
 
 const YouTubePage = () => {
@@ -11,21 +11,24 @@ const YouTubePage = () => {
     useEffect(() => {
         const fetchVideos = async () => {
           
-            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=milesdavis&key=${KEY}&part=snippet&type=video&maxResults=6`);
-            console.log(response.data);
-            setVideos(response.data);
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=milesdavis&key=AIzaSyD5lenf3Fg754H5O5CaUtYYA37XbpZQ4X8&part=snippet&type=video&maxResults=6`);
+            console.log("Youtube Data",response.data.items);
+            setVideos(response.data.items);
         };
         fetchVideos();
-    });
+    },[]);
 
     return (
         <div className="container">
       <h1>Videos</h1>
       {videos &&
         videos.map((video) => (
-            <p key={video.id}>
-                {video.snippet.thumbnails}
-            </p>
+            <>
+            <h1>{video.snippet.title}</h1>
+            <img src={video.snippet.thumbnails.default}/>
+            </>
+            
+
         ))}
     </div>
     );
